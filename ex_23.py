04063e2248085ad2f5da6cd5aa76d922941a983a
Jewon <40763988+gaw3568@users.noreply.github.com>
2021-10-21 21:53:02 +0900

    Python Coding Practice 1일차

---
+++ ex_23.py
@@ -0,0 +1,8 @@
+sleepSong_firstLine = input()
+
+print(len(sleepSong_firstLine))
+
+start_index = int(input("start_index number : "))
+end_index = int(input("end_index number : "))
+
+print(sleepSong_firstLine[start_index:end_index])
